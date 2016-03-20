4b3a1c916ab8e77fb1c74fe8b350cc3322ca8b0c
wsy <14302010062@fudan.edu.cn>
2016-03-20 18:29:58 +0800

    test

--- Lab1/src/lab1/Beverage.java
+++ Lab1/src/lab1/Beverage.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Beverage {
 	protected SizeFactor sizeFactor;
-    public int wsy;
+
 	public double cost() {
 		return 0;
 	}
--- Lab1/src/lab1/SizeFactor.java
+++ Lab1/src/lab1/SizeFactor.java
@@ -1,5 +1,6 @@
 package lab1;
 
 public interface SizeFactor {
+	String[] sizes = {"small", "medium", "large", "grant"};
 	public double sizeCost(String size);
 }
